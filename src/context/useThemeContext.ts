5f562bcb79dc7c71abbc6b90178e9b454a32b173
tepliydom91-lang <tepliy.dom91@gmail.com>
2026-09-19 11:06:42 +0200

    theme

---
+++ src/context/useThemeContext.ts
@@ -0,0 +1,15 @@
+import { useContext } from 'react'
+
+import { ThemeContext } from './ThemeContext'
+
+export const useThemeContext = () => {
+  const context = useContext(ThemeContext)
+
+  if (!context) {
+    throw new Error(
+      'useThemeContext must be used inside ThemeContextProvider',
+    )
+  }
+
+  return context
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import { ThemeContextProvider } from './context/ThemeContextProvider.tsx'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './routes/index.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeContextProvider>
+      <RouterProvider router={router} />
+    </ThemeContextProvider>
   </StrictMode>,
 )
